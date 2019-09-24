b9ac5da1fb70c30662e4f572d7c121ac7447b58e
Khismatov.ilnaz <ilnazik95@gmail.com>
2019-09-24 15:58:44 +0300

    condition added, no more than 3000

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ console.log('worked');
 // increasedСashback - 5%
 // specialCashback - 30%
 // but cashback should not exceed 3000 rubles
-const purchaseAmount = 3000;
-const simpleCasheback = purchaseAmount * 0.01;
-const increasedСashback = purchaseAmount * 0.05;
-const specialCashback = purchaseAmount * 0.3;
+const purchaseAmount = 5000;
+
+let simpleCasheback = 0;
+    if (purchaseAmount > 300000){
+        simpleCashebac = 3000;
+    } else {
+        simpleCasheback = purchaseAmount * 0.01;
+    }
+
+    let increasedСashback = 0;
+    if (purchaseAmount > 60000) {
+        increasedСashback = 3000;
+    } else {
+        increasedСashback = purchaseAmount * 0.05;
+    }
+
+let specialСashback = 0;
+    if (purchaseAmount > 10000){
+        specialCashback = 3000;
+    } else {
+        specialCashback = purchaseAmount * 0.3;
+    }
+
 console.log(simpleCasheback);
 console.log(increasedСashback);
 console.log(specialCashback);
