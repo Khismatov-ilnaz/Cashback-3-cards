92f70fc6db12792416fda26b6f628bcee6f891d4
Khismatov.ilnaz <ilnazik95@gmail.com>
2019-09-24 15:16:01 +0300

    added 3 cards

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('worked');
+// simpleCasheback - 1%
+// increasedСashback - 5%
+// specialCashback - 30%
+// but cashback should not exceed 3000 rubles
+const purchaseAmount = 3000;
+const simpleCasheback = purchaseAmount * 0.01;
+const increasedСashback = purchaseAmount * 0.05;
+const specialCashback = purchaseAmount * 0.3;
+console.log(simpleCasheback);
+console.log(increasedСashback);
+console.log(specialCashback);
