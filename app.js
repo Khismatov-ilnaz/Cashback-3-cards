193322752b33aff5b0fd8d88e6b4c3fbffac50b9
Khismatov.ilnaz <ilnazik95@gmail.com>
2019-09-25 19:46:16 +0300

    Выровнял код (#2)

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ console.log('worked');
 // increased - 5%
 // special - 30%
 // but cashback should not exceed 3000 rubles
-const amount = 233;
+const amount = 52341;
 
 let simple = 0;
-    if (amount > 300000){
-        simple = 3000;
-    } else {
-        simple = amount * 0.01;
-    }
+if (amount > 300000){
+simple = 3000;
+} else {
+simple = amount * 0.01;
+}
 
-    let increased = 0;
-    if (amount > 60000) {
-        increased = 3000;
-    } else {
-        increased = amount * 0.05;
-    }
+let increased = 0;
+if (amount > 60000) {
+increased = 3000;
+} else {
+increased = amount * 0.05;
+}
 
-    let specialСashback = 0;
-    if (amount > 10000){
-        special = 3000;
-    } else {
-        special = amount * 0.3;
-    }
+let specialСashback = 0;
+if (amount > 10000){
+special = 3000;
+} else {
+special = amount * 0.3;
+}
 
 console.log(simple);
 console.log(increased);
